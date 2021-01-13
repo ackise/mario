2e5e0d38c680c3037951afa69670ca00f6174edc
BeKateryna <begantseva.katerina@gmail.com>
2021-01-13 10:55:40 +0200

    headerbackground

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import NavBar from './NavBar/NavBar'
 import Logo from "./Logo/logo";
 import LangBlock from "./Lang/LangBlock";
@@ -6,17 +6,29 @@ import Sidebar from "./SideBar/Sidebar";
 import './Header.scss';
 
 const Header = () => {
+    const [headerColor, setHeaderColor] = useState(false);
+
+    const changeBackground = () => {
+        if (window.scrollY >= 100) {
+            setHeaderColor(true);
+        } else {
+            setHeaderColor(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeBackground)
+    }, [changeBackground])
 
     return (
-        <div className='header_wrapper'>
-        <div className="header_container">
-            <Logo />
-            <NavBar/>
-            <LangBlock style='language'/>
-            <Sidebar/>
+        <div className={headerColor ? 'header_wrapper active' : 'header_wrapper'}>
+            <div className="header_container">
+                <Logo/>
+                <NavBar/>
+                <LangBlock style='language'/>
+                <Sidebar/>
+            </div>
         </div>
-        </div>
-
     );
 };
 
--- src/components/Header/Logo/logo.js
+++ src/components/Header/Logo/logo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import LogoPic from '../../../assets/img/mario-logo.svg';
 import LogoName from '../../../assets/img/mario-logo-name.svg';
 import "./Logo.scss"
@@ -7,10 +7,10 @@ import "./Logo.scss"
 const Logo = () => {
     return (
         <div className='logo_container'>
-        <Link to="/" className='navbar_logo'>
-            <img src={LogoPic} alt="Logo" className="navbar_logo_pic"/>
-        </Link>
-            <Link to="/" className='navbar_logo' >
+            <Link to="/" className='navbar_logo'>
+                <img src={LogoPic} alt="Logo" className="navbar_logo_pic"/>
+            </Link>
+            <Link to="/" className='navbar_logo'>
                 <img src={LogoName} alt="LogoName" className="navbar_logo_name"/>
             </Link>
         </div>
